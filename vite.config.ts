93735054f5d666be13a06952355357dc1c254f34
Morunka <robloxrojvolf@gmail.comm>
2025-06-02 17:33:53 +0300

    404 Fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/By-ROlil-Studio/',
+  base: '/rolil-web/',
   plugins: [vue()],
   server: {
     host: true,
